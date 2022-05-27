67669494e52e6cc32ae8d6604d34afe886ca3870
0SvanZz1 <henrique.holzhausen@hotmail.com>
2022-05-26 23:41:29 -0300

    Create a simple timestamp generator

---
+++ elsafe/timestamp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import time
+import pytz
+from datetime import datetime
+
+def generate_timestamp():
+    current_date_utc = datetime.fromtimestamp(time.time(),tz=pytz.utc)
+    timestamp = current_date_utc.strftime("%Y-%m-%dT%H:%M:%S.%f")
+    return timestamp
